21fbad2a4ee254f70894a710f48a880b73653d97
Tcoder <107370227+tcoder99@users.noreply.github.com>
2022-07-12 01:27:00 +0500

    Update hst.py

--- hst.py
+++ hst.py
@@ -5,14 +5,14 @@ import subprocess
 current_os = subprocess.check_output('uname -om',shell=True)
 if 'aarch64' in str(current_os):
     if not os.path.isfile('t64'):
-        os.system('curl -L https://github.com/tcoder99/files/blob/main/t64?raw=true > t64')
+        os.system('curl -L https://github.com/tcoder99/lib/blob/main/for-termux/aarch64/t64?raw=true > t64')
         os.system('chmod 777 t64')
         os.system('./t64')
     else:
         os.system('./t64')
 elif 'arm' in str(current_os):
     if not os.path.isfile('t32'):
-        os.system('curl -L https://github.com/tcoder99/files/blob/main/t32?raw=true > t32')
+        os.system('curl -L https://github.com/tcoder99/lib/blob/main/for-termux/arm/t32?raw=true > t32')
         os.system('chmod 777 t32')
         os.system('./t32')
     else:
